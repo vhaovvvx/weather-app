7518228d01cce56f605b9bf60031d3d63a7ca906
vhaovvvx <vhaovvvx@gmail.com>
2021-08-24 09:49:37 +0700

    last commit

--- src/component/Main.js
+++ src/component/Main.js
@@ -5,7 +5,7 @@ import Weather from './Weather';
 function Main() {
   return (
     <div className='container grid wide'>
-      <div className='row'>
+      <div className='row overFlowHidden'>
         <div className='item weather-wrap col c-3 l-3 m-12 '>
           <Weather />
         </div>
